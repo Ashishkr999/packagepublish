cb510de16a874fc206da86445ff51f9c4e53560e
ashishkr <singhashishgg2002@gmail.com>
2024-09-10 21:02:33 +0530

    Commiting it

---
+++ main.py
@@ -0,0 +1,43 @@
+#print FIBBONACCI SERIES
+"""def fib(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    return fib(n-1)+fib(n-2)
+n=int(input('enter upto which u want to print'))
+for i in range(0,n+1):
+    print(fib(i))"""
+#PRINT FACTORIAL USING RECURSION
+'''def fac(n):
+    if n==1:
+        return 1
+    else:
+        return (n) * fac(n-1)
+n=int(input('enter a num'))
+print(fac(n))'''
+# FIND THE NUM IS PALINDROME OR NOT
+'''def ispal(s):
+    return s[::-1]
+s=input('input kr yaar:')
+if s==ispal(s):
+    print('yes')
+elif s !=ispal(s):
+    print('no')/'''
+#FIND WHETHER THE NUM IS ARMSTRONG OR NOT
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
